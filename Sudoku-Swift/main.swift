027064cc03468d1c9f09e425699ff9dec7ec8299
Isak Sky <isak.sky@gmail.com>
2014-11-23 18:36:47 -0800

    Initial Commit

---
+++ Sudoku-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sudoku-Swift
+//
+//  Created by Isak on 11/23/14.
+//  Copyright (c) 2014 Isak Sky. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
